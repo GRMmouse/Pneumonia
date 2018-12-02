3d560a36924d27bcd774ae67ed54bf5c8667bede
GRMmouse <ail1@andrew.cmu.edu>
2018-12-02 14:21:25 -0500

    Style: added headers for each section

--- code/naive_svm.m
+++ code/naive_svm.m
@@ -1,3 +1,8 @@
+% Accuracy: .7468
+% Precision: .7180
+% Recall: .9795
+
+%% Load Data
 if (~ exist("norm_data"))
     load("data.mat");
 end
@@ -9,7 +14,16 @@ trainY = [zeros(size(norm_data{3}, 1), 1); ones(size(pneu_data{3}, 1), 1)];
 testX = [norm_data{2}; pneu_data{2}];
 testY = [zeros(size(norm_data{2}, 1), 1); ones(size(pneu_data{2}, 1), 1)];
 
+%% Classification
 mdl = fitcsvm(trainX, trainY);
 disp("Finished Training");
 predY = predict(mdl, testX);
-mean(predY==testY)
+
+%% Evaluation
+tp = sum(testY==1 & predY==1);
+fp = sum(testY==0 & predY==1);
+tn = sum(testY==0 & predY==0);
+fn = sum(testY==1 & predY==0);
+acc = (tp+tn)/size(predY, 1)
+precision = tp/(tp+fp)
+recall = tp/(tp+fn)
--- code/pca_svm.m
+++ code/pca_svm.m
@@ -2,6 +2,7 @@
 % Precision: .7180
 % Recall: .9795
 
+%% Load Data
 if (~ exist("norm_data"))
     load("data.mat");
 end
@@ -13,14 +14,20 @@ trainY = [zeros(size(norm_data{3}, 1), 1); ones(size(pneu_data{3}, 1), 1)];
 testX = [norm_data{2}; pneu_data{2}];
 testY = [zeros(size(norm_data{2}, 1), 1); ones(size(pneu_data{2}, 1), 1)];
 
+%% PCA
+[score, latent, var_explained] = choose_pc(trainX, 32);
+sprintf("%.2f of total variance explained", var_explained)
+
+%% Classification
 mdl = fitcsvm(trainX, trainY);
 disp("Finished Training");
 predY = predict(mdl, testX);
 
+%% Evaluation
 tp = sum(testY==1 & predY==1);
 fp = sum(testY==0 & predY==1);
 tn = sum(testY==0 & predY==0);
 fn = sum(testY==1 & predY==0);
 acc = (tp+tn)/size(predY, 1)
 precision = tp/(tp+fp)
-recall = tp)/(tp+fn)
+recall = tp/(tp+fn)
