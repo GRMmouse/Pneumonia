80a1e612ac4bfb8413fd71cb3a8d37ffe37d731e
GRMmouse <ail1@andrew.cmu.edu>
2018-12-02 14:11:58 -0500

    Feat: Implementing svm classification with pca preprocessing

---
+++ code/pca_svm.m
@@ -0,0 +1,26 @@
+% Accuracy: .7468
+% Precision: .7180
+% Recall: .9795
+
+if (~ exist("norm_data"))
+    load("data.mat");
+end
+disp("Finished loading data");
+
+trainX = [norm_data{3}; pneu_data{3}];
+trainY = [zeros(size(norm_data{3}, 1), 1); ones(size(pneu_data{3}, 1), 1)];
+
+testX = [norm_data{2}; pneu_data{2}];
+testY = [zeros(size(norm_data{2}, 1), 1); ones(size(pneu_data{2}, 1), 1)];
+
+mdl = fitcsvm(trainX, trainY);
+disp("Finished Training");
+predY = predict(mdl, testX);
+
+tp = sum(testY==1 & predY==1);
+fp = sum(testY==0 & predY==1);
+tn = sum(testY==0 & predY==0);
+fn = sum(testY==1 & predY==0);
+acc = (tp+tn)/size(predY, 1)
+precision = tp/(tp+fp)
+recall = tp)/(tp+fn)
